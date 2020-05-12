9d07131050ef94dd35e57869f4e9319206d975d9
songlijun <github@itjh.com.cn>
2020-05-12 10:07:09 +0800

    first commit

---
+++ SLPokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SLPokeMaster
+//
+//  Created by song lijun on 2020/5/12.
+//  Copyright © 2020 宋立君. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
